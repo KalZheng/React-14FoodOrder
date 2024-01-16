1c7ed50a8cfea89024fb28069c0d8ba67cd0afb9
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-16 15:07:31 +0800

    add util button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import logoImg from "../assets/logo.jpg";
+import Button from "../UI/Button";
 
 export default function Header() {
   return (
     <header id="main-header">
       <div id="title">
-        <img src={logoImg} alt="a restaurant"/>
+        <img src={logoImg} alt="a restaurant" />
         <h1>React Food</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
