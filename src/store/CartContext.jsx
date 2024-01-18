ffbd416716468cdd4681ae08a9c175dd890c625b
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-18 15:18:38 +0800

    add useHttp into checkout component and some extra function to clear when success checkout

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -49,6 +50,10 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
+
   return state;
 }
 
@@ -62,11 +67,15 @@ export function CartContextProvider({ children }) {
   function removeItem(id) {
     dispatchAction({ type: "REMOVE_ITEM", id });
   }
+  function clearCart() {
+    dispatchAction({ type: "CLEAR_CART" });
+  }
 
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart,
   };
 
   console.log(cartContext);
