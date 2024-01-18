afa375b81a2c7a05bf18b95eda954d6e49482599
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-18 14:50:19 +0800

    add error component and check for error in meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,10 +1,22 @@
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp.js";
-
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 export default function Meals() {
-  const { data: loadedMeals, isLoading, error } = useHttp("http://localhost:3000/meals", requestConfig, []);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p className="center">Fetching Meals...</p>;
+  }
+
+  if (error) {
+    return <Error title="Failed to Fetch Meals" message={error} />;
+  }
 
   return (
     <ul id="meals">
