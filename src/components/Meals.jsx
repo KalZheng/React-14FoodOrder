15ab888a9f2753715148315bc4ca0ae7dd3e3e21
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-18 14:39:40 +0800

    add the useHttp into meals and use it to fetch data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp.js";
 
+
+const requestConfig = {};
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-
-      if (!response.ok) {
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  const { data: loadedMeals, isLoading, error } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
   return (
     <ul id="meals">
